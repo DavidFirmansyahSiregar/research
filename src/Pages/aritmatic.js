3a396460ff4e9dd375fb2ccb5897bd14ae1836f9
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-01 15:12:51 +0700

    aricmatic

--- src/Pages/aritmatic.js
+++ src/Pages/aritmatic.js
@@ -1,7 +1,11 @@
 import React from 'react'
 
 export const aritmatic = () => {
+  let a = 3
+  let x = (100 + 50) * a
   return (
-    <div>aritmatic</div>
+    <div>
+      x = 450
+    </div>
   )
 }
