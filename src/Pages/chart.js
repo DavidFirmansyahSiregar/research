ea481eaa291191a10920da61d3db38c6f2de61bb
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-01 15:20:57 +0700

    chart

--- src/Pages/chart.js
+++ src/Pages/chart.js
@@ -1,7 +1,45 @@
 import React from 'react'
+import './pages.css';
 
-export const chart = () => {
+import { Line } from 'react-chartjs-2';
+import {
+  Chart,
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+} from 'chart.js';
+
+Chart.register(
+  LineElement,
+  CategoryScale,
+  LinearScale,
+  PointElement
+)
+
+export const Data = () => {
+  const data = {
+    labels:["Nov 1","Nov 2","Nov 3","Nov 4","Nov 5","Nov 6","nov 7","Nov 8", "Nov 9", "Nov 10"],
+    datasets: [{
+      data: [8, 7.8, 6, 8, 7, 5, 6, 4, 7.2],
+      borderColor: 'f26c6d',
+      pointBorderColor: 'white',
+    },
+    { data: [5, 8, 7, 6.6, 6, 7, 4, 7, 5.5],
+      borderColor: '#F4898A',
+      pointBorderColor: 'red',}
+  ],
+  }
+    const handleInput = {
+
+    }
+
   return (
-    <div>chart</div>
+    <div>
+      <div className='chart-1'>
+        <Line data={data}></Line>
+      </div>    
+    </div>
+
   )
 }
