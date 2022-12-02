1eb4fe1b910cbc142b2a9ebbe7dc2386915fdc9b
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-02 09:53:32 +0700

    prog

---
+++ src/Pages/progress.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Progress, Space } from 'antd';
+export const progress = () => (
+  <>
+    <Progress
+      percent={99.9}
+      strokeColor={{
+        '0%': '#108ee9',
+        '100%': '#87d068',
+      }}
+    />
+    <Progress
+      percent={99.9}
+      status="active"
+      strokeColor={{
+        from: '#108ee9',
+        to: '#87d068',
+      }}
+    />
+    <Space wrap>
+      <Progress
+        type="circle"
+        percent={90}
+        strokeColor={{
+          '0%': '#108ee9',
+          '100%': '#87d068',
+        }}
+      />
+      <Progress
+        type="circle"
+        percent={100}
+        strokeColor={{
+          '0%': '#108ee9',
+          '100%': '#87d068',
+        }}
+      />
+    </Space>
+  </>
+);
