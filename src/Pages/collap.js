738045bcb3753bfe07f267c10ec3e4fb3d36ae49
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-05 09:48:14 +0700

    collapse

--- src/Pages/collap.js
+++ src/Pages/collap.js
@@ -2,7 +2,30 @@ import React from 'react';
 import './pages.css'
 
 export const collap = () => {
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+            content.style.display = "block";
+            }
+        });
+    }
   return (
-    <div>collap</div>
+    <div>
+        <div className="content">
+            <button type="button" className="collapsible">Open Collapsible</button>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+        <div className="content">
+            <button type="button" className="collapsible">Open Section 2</button>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+        </div>
+    </div>
   )
 }
