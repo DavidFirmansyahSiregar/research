83ab571cb9c1a4e3775adebbbbbc07a26041fce5
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-05 22:33:33 +0700

    news

--- src/Pages/newslatter.js
+++ src/Pages/newslatter.js
@@ -7,18 +7,19 @@ export const newslatter = () => {
         <h2>CSS Newsletter</h2>
 
     <form action="/action_page.php">
-      <div class="container">
+      <div className="container">
         <h2>Subscribe to our Newsletter</h2>
         <p>Lorem ipsum text about why you should subscribe to our newsletter blabla. Lorem ipsum text about why you should subscribe to our newsletter blabla.</p>
       </div>
     
-      <div class="container" style="background-color:white">
-        <input type="text" placeholder="Name" name="name" required>
-        <input type="text" placeholder="Email address" name="mail" required>
+      <div className="container">
+        <input type="text" placeholder="Name" name="name" required></input>
+        <input type="text" placeholder="Email address" name="mail" required></input>
         <label>
-          <input type="checkbox" checked="checked" name="subscribe"> Daily Newsletter
+          <input type="checkbox" checked="checked" name="subscribe"> Daily Newsletter</input>
         </label>
       </div>
+    </form>
     </div>
   )
-}</form></div>)}
+}
