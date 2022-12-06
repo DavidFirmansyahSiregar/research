0b8f54555a83a73af36304ea195dd63cc7284c19
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-06 12:11:34 +0700

    imageside

---
+++ src/Pages/imageslider.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './pages.css';
+
+export const imageslider = () => {
+  return (
+    <div class="img-comp-container">
+  <div class="img-comp-img">
+    <img src="img_snow.jpg" width="300" height="200"/>
+  </div>
+  <div class="img-comp-img img-comp-overlay">
+    <img src="img_forest.jpg" width="300" height="200"/>
+  </div>
+</div>
+  )
+}
