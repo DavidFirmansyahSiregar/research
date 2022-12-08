8689a5561d59319db6545b47365ce2038a813165
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-08 14:19:41 +0700

    tambahan

---
+++ src/Pages/OurStory.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { ourStoryText } from "./ourStoryText";
+
+export const OurStory = () => {
+    return (
+       <div>
+          <div className="headline">
+             <span className="gold">
+            <dynamic-variale>.goldTitle</dynamic-variale>
+             </span>
+             <span>
+           <dynamic-variale>.navyTitle</dynamic-variale>
+             </span>
+          </div>         
+          <dynamic-variale>.text</dynamic-variale>
+          <br />
+       </div>
+    )
+}
