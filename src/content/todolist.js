1218d88ff65b08581f72bbb2abe68437376b9154
DavidFirmansyahSiregar <davidfirmansyahsiregar@gmail.com>
2022-12-13 10:21:43 +0700

    todo

--- src/content/todolist.js
+++ src/content/todolist.js
@@ -1,6 +1,51 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export const todolist = () => {
+  const [toDo, setToDo] = useState(() => {
+    const savedTodos = localStorage.getItem("toDo");
+    if (savedTodos) {
+      return JSON.parse(savedTodos);
+    } else {
+      return [];
+    }
+  });
+
+  const [newTask, setNewTask] = useState("");
+  const [updateData, setUpdateData] = useState("");
+
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
+
+  const deleteTask = (id) => {
+    let newTasks = toDo.filter((task) => task.id !== id);
+    setToDo(newTasks);
+  };
+
+  const cancelUpdate = () => {
+    setUpdateData("");
+  };
+
+  const changeTask = (e) => {
+    let newEntry = {
+      id: updateData.id,
+      title: e.target.value,
+      status: updateData.status ? true : false,
+    };
+    setUpdateData(newEntry);
+  };
+  
+  const updateTask = () => {
+    let filterRecords = [...toDo].filter((task) => task?.id !== updateData.id);
+    let updatedObject = [...filterRecords, updateData];
+    setToDo(updatedObject);
+    setUpdateData("");
+  };
   return (
     <div>todolist</div>
   )
